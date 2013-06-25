5382388132abb971e5e0071ddc6526f1b8014330
Dmitry Kabanov <kabanovdmitry@gmail.com>
2013-06-25 20:34:25 +0300

    Extract module fcns from dummy.f90 into separate file.

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 all:
 	gfortran -c vode.f
+	gfortran -c fcns.f90
 	gfortran -c dummy.f90
--- dummy.f90
+++ dummy.f90
@@ -1,27 +1,3 @@
-module fcns
-contains
-subroutine lambda_fcn(n, x, lambda, rhs, rp, ip)
-    ! Computes the RHS of the ODE: dl/dx = k*(1-lambda)*exp(-e/(p*v))/u
-    integer, intent(in) :: n
-    double precision, intent(in) :: x, lambda(1)
-    double precision, intent(out) :: rhs(1)
-    double precision, intent(inout) :: rp(1)
-    integer, intent(inout) :: ip(1)
-end subroutine lambda_fcn
-
-subroutine dummy_jac(NEQ, T, Y, ML, MU, PD, NROWPD, RPAR, IPAR)
-    integer, intent(in) :: NEQ
-    double precision, intent(in) :: T
-    double precision, intent(in) :: Y(NEQ)
-    integer, intent(in) :: ML
-    integer, intent(in) :: MU
-    integer, intent(in) :: NROWPD
-    double precision, intent(inout) :: PD(NROWPD,NEQ)
-    double precision, intent(inout) :: RPAR(:)
-    integer, intent(inout) :: IPAR(:)
-end subroutine dummy_jac
-end module
-
 program dummy
     use vode_int
     use fcns
